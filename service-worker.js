d8fbe626365bf08e8c22f92701a50f4010e4448d
Roman Radaykin <roman.radaikin@yandex.ru>
2021-01-12 13:17:46 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/simple-react-homeworks/precache-manifest.7ea51be915cad1fdea9a128903978df4.js"
+  "/simple-react-homeworks/precache-manifest.77db2fa62c2c339280671d998da5d220.js"
 );
 
 self.addEventListener('message', (event) => {
