0d9fc89bdf5983e06c5d241c1d9a65953f1f052e
Roman Radaykin <roman.radaikin@yandex.ru>
2021-01-16 14:10:17 +0300

    '6_homework_done'

--- src/p2-homeworks/h5/pages/PreJunior.tsx
+++ src/p2-homeworks/h5/pages/PreJunior.tsx
@@ -4,6 +4,7 @@ import HW2 from "../../h2/HW2";
 import HW3 from "../../h3/HW3";
 import HW4 from "../../h4/HW4";
 import HW5 from "../HW5";
+import HW6 from "../../h6/HW6";
 
 function PreJunior() {
     return (
@@ -13,7 +14,7 @@ function PreJunior() {
             <HW3/>
             <HW4/>
             {/*<HW5/>*/}
-            {/*<HW6/>*/}
+            <HW6/>
 
         </div>
     );
