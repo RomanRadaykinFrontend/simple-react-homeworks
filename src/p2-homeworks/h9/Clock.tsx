099ccd83da73624b052fe6c7f46df8c4e47399a6
Roman Radaykin <roman.radaikin@yandex.ru>
2021-02-07 18:53:28 +0300

    'hw_9'

--- src/p2-homeworks/h9/Clock.tsx
+++ src/p2-homeworks/h9/Clock.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useState, MouseEvent} from "react";
 import SuperButton from "../h4/common/c2-SuperButton/SuperButton";
 
 function Clock() {
@@ -8,24 +8,33 @@ function Clock() {
 
     const stop = () => {
         // stop
+        clearInterval(timerId)
     }
     const start = () => {
         stop();
         const id: number = window.setInterval(() => {
             // setDate
+            let newDate = new Date();
+            setDate(newDate)
         }, 1000);
         setTimerId(id);
     }
 
-    const onMouseEnter = () => {
+    const onMouseEnter = (e: MouseEvent) => {
         // show
+        if(e.type === 'mouseenter'){
+            setShow(true)
+        }
     };
-    const onMouseLeave = () => {
+    const onMouseLeave = (e: MouseEvent) => {
         // close
+        if(e.type === 'mouseleave'){
+            setShow(false)
+        }
     };
 
-    const stringTime = "Time"; // fix with date
-    const stringDate = "Date"; // fix with date
+    const stringTime = date ? `${date.getHours()} : ${date.getMinutes()} : ${date.getSeconds()}` : ''; // fix with date
+    const stringDate = date ? `${date.getDate()}.${date.getMonth() + 1}.${date.getFullYear()} `: ''; // fix with date
 
     return (
         <div>
